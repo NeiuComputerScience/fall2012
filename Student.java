0c472ef0cf3caccf72cca09b03400412c81fe8e9
Diego A. Cruz <diego.alicruz9@gmail.com>
2012-11-01 13:29:32 -0500

    This is a test
    delete all

---
+++ Student.java
@@ -0,0 +1,34 @@
+/*
+*
+* Author: Troy
+* Author: Diego
+* Author: Rayed
+* Author: Chris
+*---------------
+* Used to create Student objects with names and IDs.
+*/
+public class Student{
+
+	private String firstName;
+	private String lastName;
+	private int studentID;
+	private char studentgrade;
+	
+	public Student(String fN, String lN, int iD){
+		firstName = fN;
+		lastName = lN;
+		studentID = iD;
+	}
+	public String getFN(){
+		return(firstName);
+	}
+	public String getLN(){
+		return(lastName);
+	}
+	public int getID(){
+		return(studentID);
+	}
+	public char calculateGrade()
+	{
+	}
+}
